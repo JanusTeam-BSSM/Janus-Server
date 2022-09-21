d6fc072d707ddd576ba06dccd304f7a56653e495
diadntjr <dntjr0501@gmail.com>
2022-09-21 09:09:29 +0900

    Update app.js
    Change render part

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, function() {
-    console.log("서버 가동");
-});
-
-app.get('/', function(request, response) {
-    response.send("여기는 루트입니다.");
-});
-
-app.get('/login', function(request, response) {
-    response.send("여기는 로그인 화면입니다.");
-});
-
 app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.get("/", function(request, response) {
+    response.render("home/index");
+});
+
+app.get("/login", function(request, response) {
+    response.render("home/login");
+})
